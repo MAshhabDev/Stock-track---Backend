3047ca28a2dbd759e735184465488126e36a04f6
Mahir4098 <mahirjr98@gmail.com>
2026-08-24 20:41:12 +0600

    feat: implement auth middleware

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 import { Role } from '../../../generated/prisma/enums'
 import { AuthController } from './auth.controller'
+import { auth } from '../../middleware/checkAuth'
 
 const router = Router()
 
-router.post('/register', AuthController.registerPatient)
+router.post('/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
+
+router.get(
+    '/me',
+    auth(Role.ADMIN,Role.STAFF),
+    AuthController.getMe,
+)
 
 router.post('/refresh-token', AuthController.refreshToken)
 export const AuthRoutes = router
